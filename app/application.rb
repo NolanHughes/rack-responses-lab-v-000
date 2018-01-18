44d35e11832c9a931188e57e1c33d1db6612babc
Nolan Hughes <nhughes987@gmail.com>
2018-01-18 15:57:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     resp.write "Hello, World."
-    resp.write "The time is ____"
+    resp.write "\nThe time is ____"
     resp.finish
   end
 end
