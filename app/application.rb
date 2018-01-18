abdbc74c633ed7827972eb51731e664070ba0473
Nolan Hughes <nhughes987@gmail.com>
2018-01-18 16:05:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,7 @@ class Application
     # resp.write "Hello, World."
     # resp.write "\nThe time is ____"
     if Time.now.hour < 12
+      binding.pry
       resp.write "Morning"
     else
       resp.write "Afternoon"
