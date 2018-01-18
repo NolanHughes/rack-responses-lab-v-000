73164987d1eb265ba8d8366ec24795183d9cb3de
Nolan Hughes <nhughes987@gmail.com>
2018-01-18 16:09:37 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     resp = Rack::Response.new
     # resp.write "Hello, World."
     # resp.write "\nThe time is ____"
-    if Time.now.localtime.hour < 18
+    if Time.now.localtime.hour < 12
       # binding.pry
       resp.write "Morning"
       resp.write "\nIt is now the hour of #{Time.now.localtime.hour}"
