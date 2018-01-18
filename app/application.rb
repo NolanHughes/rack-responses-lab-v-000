8717437c854de59fcbbb1361a3ce8ff0850a362a
Nolan Hughes <nhughes987@gmail.com>
2018-01-18 16:12:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,13 +5,13 @@ class Application
     resp = Rack::Response.new
     # resp.write "Hello, World."
     # resp.write "\nThe time is ____"
-    if Time.now.localtime.hour < 18
+    if Time.now.getlocal.hour < 18
       # binding.pry
       resp.write "Morning"
-      resp.write "\nIt is now the hour of #{Time.now.localtime.hour - 6}"
+      resp.write "\nIt is now the hour of #{Time.now.getlocal.hour - 6}"
     else
       resp.write "Afternoon"
-      resp.write "\nIt is now the hour of #{Time.now.localtime.hour - 6}"
+      resp.write "\nIt is now the hour of #{Time.now.getlocal.hour - 6}"
     end
 
     resp.finish
