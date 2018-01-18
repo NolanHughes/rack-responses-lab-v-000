764591b74b72cfa16e4da092da2d21d0f1a0aa8c
Nolan Hughes <nhughes987@gmail.com>
2018-01-18 16:16:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,13 +6,13 @@ class Application
 
     current_central_time = Time.now.getlocal
     binding.pry
-    if current_central_time.hour < 18
+    if current_central_time.hour < 12
       # binding.pry
       resp.write "Morning"
-      resp.write "\nIt is now the hour of #{Time.now.getlocal.hour - 6}"
+      resp.write "\nIt is now the hour of #{Time.now.getlocal.hour}"
     else
       resp.write "Afternoon"
-      resp.write "\nIt is now the hour of #{Time.now.getlocal.hour - 6}"
+      resp.write "\nIt is now the hour of #{Time.now.getlocal.hour}"
     end
 
     resp.finish
