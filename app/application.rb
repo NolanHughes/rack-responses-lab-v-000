3f716787085cdfb359eded9b91feec506452fe98
Nolan Hughes <nhughes987@gmail.com>
2018-01-18 16:08:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,10 +8,10 @@ class Application
     if Time.now.localtime.hour < 12
       # binding.pry
       resp.write "Morning"
-      resp.write "\nIt is now the hour of #{Time.now.hour}"
+      resp.write "\nIt is now the hour of #{Time.now.localtime.hour}"
     else
       resp.write "Afternoon"
-      resp.write "\nIt is now the hour of #{Time.now.hour}"
+      resp.write "\nIt is now the hour of #{Time.now.localtime.hour}"
     end
 
     resp.finish
